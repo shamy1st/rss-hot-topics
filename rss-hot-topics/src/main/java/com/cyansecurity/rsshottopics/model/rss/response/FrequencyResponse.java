54c95d161e97ab146d3e13faccc9dc684a4c94ef
Ahmed Elshamy <shamy1st@icloud.com>
2021-06-15 07:51:04 +0200

    Delete rss-hot-topics directory

--- rss-hot-topics/src/main/java/com/cyansecurity/rsshottopics/model/rss/response/FrequencyResponse.java
+++
@@ -1,8 +0,0 @@
-/***
- ** Copyright 2021, Ahmed Elshamy, shamy1st@icloud.com, All rights reserved.
- **/
-package com.cyansecurity.rsshottopics.model.rss.response;
-
-public interface FrequencyResponse {
-
-}
